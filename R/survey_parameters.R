6a929061d3d4c07bf7778a01bb6c7c78817ed05b
Jae S Choi <choi.jae.seok@gmail.com>
2021-04-19 13:34:43 -0300

    try reduced intercept pamaters

--- R/survey_parameters.R
+++ R/survey_parameters.R
@@ -108,10 +108,12 @@ survey_parameters = function( p=NULL, project_name=NULL, project_class="core", .
             ' + f( inla.group( t, method="quantile", n=9 ), model="rw2", scale.model=TRUE, hyper=H$rw2) ',
             ' + f( inla.group( z, method="quantile", n=9 ), model="rw2", scale.model=TRUE, hyper=H$rw2) ',
             ' + f( inla.group( substrate.grainsize, method="quantile", n=9 ), model="rw2", scale.model=TRUE, hyper=H$rw2) ',
-            ' + f( auid_main, model="bym2", graph=slot(sppoly, "nb"), scale.model=TRUE, constr=TRUE, hyper=H$bym2 ) ',
+            ' + f( auid_main, model="besag", graph=slot(sppoly, "nb"), scale.model=TRUE, constr=TRUE ) ',
             ' + f( auid, model="bym2", graph=slot(sppoly, "nb"), group=year_factor, scale.model=TRUE, constr=TRUE, hyper=H$bym2, control.group=list(model="ar1", hyper=H$ar1_group)) '
         ))
-       }
+       
+      }
+      
       if ( !exists("carstm_model_family", p)  )  p$carstm_model_family = "normal"
     }
 
