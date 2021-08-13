37feb7f134b07b7fbe00bce74dc1a9dd07b23a56
Jae S Choi <choi.jae.seok@gmail.com>
2021-08-13 19:07:38 -0300

    move carstm data prep functions

--- R/survey_parameters.R
+++ R/survey_parameters.R
@@ -55,6 +55,26 @@ survey_parameters = function( p=NULL, project_name=NULL, project_class="core", .
     season = "summer"
   )
 
+  p = parameters_add_without_overwriting( p, discretization=list() )
+  p$discretization = parameters_add_without_overwriting( p$discretization,
+    z = c(0, 10, 20, 40, 80, 100, 150, 200, 250, 300, 350, 400, 500, 1000, 2000, 5000, 8000 ),
+    dz = c(0.01, 0.1,  1, 2, 4, 6, 8, 10, 12 ),
+    ddz = c(0.01, 0.1, 0.2, 0.4, 0.8, 1, 2, 4  ),
+    substrate.grainsize = c( 0, 1, 2, 4, 8, 12, 16, 20, 32 ),
+    pca1 = seq( -1, 1, by=0.1  ),
+    pca2 = seq( -1, 1, by=0.1  ),
+    t = seq( -4, 25, by=1  ),
+    tsd = seq( 0, 25, by=1  ),
+    tmin = seq( -4, 25, by=1  ),
+    tmax = seq( -4, 25, by=1  ),
+    degreedays = c(10, 100, 200, 400, 800, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 5000),
+    dyear = seq( 0, 1, by=0.1  )
+  )
+
+  p$discretization = parameters_add_without_overwriting( p$discretization,
+    n.season = length(p$discretization[["dyear"]]) - 1   # used by seasonal error in inla ... not really useful
+  )
+
 
   if (project_class=="core") {
     return(p)
