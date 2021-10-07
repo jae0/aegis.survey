0993c1f9d7a274a57d6d5fad8f6185c096693dd9
Jae S Choi <choi.jae.seok@gmail.com>
2021-10-07 08:19:16 -0300

    prep for automatioon

---
+++ R/aegis_survey_index.R
@@ -0,0 +1,35 @@
+
+
+
+aegis_survey_index = function( object, p, set=NULL, ... ) {
+
+  # ---------------------
+  # deal with additional passed parameters
+  p = parameters_add(p, list(...) ) # add passed args to parameter list, priority to args
+
+
+
+  if (object=="stratanal") {
+
+
+  }
+
+  if (object=="meansize") {
+
+
+  }
+
+  if (object=="abundance") {
+
+
+  }
+
+
+  if (object=="habitat") {
+
+
+  }
+
+
+
+}
